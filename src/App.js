fc986f456a208f811d9c7cd30cf89cdec407e1db
jackson ford <jfordnose64@gmail.com>
2019-11-30 10:56:16 -0500

    Before Tristan

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import Home from './components/Home'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return <Home />
   }
 }
 
